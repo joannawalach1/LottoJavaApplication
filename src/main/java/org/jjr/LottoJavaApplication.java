aee338384d4339189269cf200277382f81744a35
joannawalach1 <joanna.walach@op.pl>
2025-08-11 12:20:39 +0200

    basic structure

---
+++ src/main/java/org/jjr/LottoJavaApplication.java
@@ -0,0 +1,12 @@
+package org.jjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoJavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LottoJavaApplication.class, args);
+
+    }
+}
